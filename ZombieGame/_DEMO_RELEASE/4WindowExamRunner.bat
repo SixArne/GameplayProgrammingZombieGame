50a15ffae3181f4cb81da2c90e56924729789e80
arne six <six.arne@gmail.com>
2023-01-15 13:06:34 +0100

    finished powerpoint

--- ZombieGame/_DEMO_RELEASE/4WindowExamRunner.bat
+++ ZombieGame/_DEMO_RELEASE/4WindowExamRunner.bat
@@ -1,9 +1,9 @@
-start GPP_TEST_RELEASE.exe -x 30 -y 30 -s 69
+start GPP_TEST_RELEASE.exe -x 30 -y 30 -s 0
 
-start GPP_TEST_RELEASE.exe -x 1000 -y 30 -s 420
+start GPP_TEST_RELEASE.exe -x 1000 -y 30 -s 0
 
-start GPP_TEST_RELEASE.exe -x 30 -y 540 -s 69420
+start GPP_TEST_RELEASE.exe -x 30 -y 540 -s 0
 
-start GPP_TEST_RELEASE.exe -x 1000 -y 540 -s 21
+start GPP_TEST_RELEASE.exe -x 1000 -y 540 -s 0
 echo new ActiveXObject("WScript.Shell").AppActivate("GPP_TEST_RELEASE.exe"); > tmp.js
 cscript //nologo tmp.js & del tmp.js
